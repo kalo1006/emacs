6bd6c8906bdaf75e2e8f8df53c2a4c67b80135ba
kalo1006 <r201704236rw@jindai.jp>
2018-05-06 16:26:23 +0900

    emacs name

---
+++ init.el
@@ -0,0 +1,30 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(electric-pair-mode 1)
+(show-paren-mode t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (company))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(require 'company)
+(global-company-mode) 
+(setq company-idle-delay 0.5) 
+(setq company-minimum-prefix-length 2) 
+(setq company-selection-wrap-around t) 
+(setq c-auto-newline t) 
+(setq kill-whole-line t)
+(setq electric-indent-mode 1)
