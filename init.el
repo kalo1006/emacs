34c8d5b6f818e481bdc726c6ec604cacc8f210e9
kalo1006 <38834378+kalo1006@users.noreply.github.com>
2018-06-09 21:21:24 +0900

    Add files via upload

--- init.el
+++ init.el
@@ -1,30 +1,61 @@
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
 (package-initialize)
 
 (electric-pair-mode 1)
 (show-paren-mode t)
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (company))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+ '(package-selected-packages (quote (company-irony-c-headers company))))
+(custom-set-faces)
+
+(add-to-list 'load-path "~/styleguide-gh-pages/")
+(require 'google-c-style)
+(add-hook 'c-mode-common-hook 'google-set-c-style)
+(add-hook 'c-mode-common-hook 'google-make-newline-indent)
+
 
 (require 'company)
 (global-company-mode) 
 (setq company-idle-delay 0.5) 
 (setq company-minimum-prefix-length 2) 
-(setq company-selection-wrap-around t) 
+(setq company-selection-wrap-around t)
+
+
+(require 'irony)
+(add-hook 'c-mode-hook 'irony-mode)
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'objc-mode-hook 'irony-mode)
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+(add-to-list'company-backends 'company-irony)
+
+;;(require 'company-c-headers)
+;;(add-to-list 'company-backends 'company-c-headers)
+;;(require 'company-irony)
+;;(add-to-list 'company-backends 'company-irony)
+;;(require 'company-yasnippet)
+;;(add-to-list 'company-backends 'company-yasnippet)
+(require 'company-irony-c-headers)
+(add-to-list 'company-backends 'company-irony-c-headers)
+
+
 (setq c-auto-newline t) 
+(setq c-hungry-delete t)
 (setq kill-whole-line t)
 (setq electric-indent-mode 1)
+(setq-default c-basic-offset 4     ;;基本インデント量4
+              tab-width 4          ;;タブ幅4
+              indent-tabs-mode nil)  ;;インデント
+
+(require 'package)
+;; package.elが管理していないelisp置き場
+(add-to-list 'load-path "~/.emacs.d/elisp/")
+;; MELPAを追加(package.el)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+;; Marmaladeを追加(package.el)
+(add-to-list 'package-archives  '("marmalade" . "http://marmalade-repo.org/packages/"))
+
+;; package.elの初期化
+(package-initialize)
+
+
+
+     
